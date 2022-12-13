fd6e64794d16114900e263be4e5d8244134747dc
Abraham Kalovsky <Kingsjester14@gmail.com>
2022-12-13 12:00:41 -0600

    initial commit

---
+++ src/main/java/RecommendationService/Magic/The/Gathering/MagicTheGatheringApplication.java
@@ -0,0 +1,13 @@
+package RecommendationService.Magic.The.Gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTheGatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicTheGatheringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RecommendationService/Magic/The/Gathering/MagicTheGatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package RecommendationService.Magic.The.Gathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTheGatheringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
